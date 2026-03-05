8657d391024a2d54afa8199214b65a3ddf4cf9db
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2026-03-05 18:32:01 -0300

    Atualizando Arquivo Verificar Maioridade

--- src/main/java/br/com/tiagopitombeira/VerificarMaioridade.java
+++ src/main/java/br/com/tiagopitombeira/VerificarMaioridade.java
@@ -2,4 +2,15 @@ package br.com.tiagopitombeira;
 
 public class VerificarMaioridade {
 
+	public static void main(String[] args) {
+		int idade = 16;
+		System.out.println("Idade: " + idade);
+		if (idade >= 18) {
+			System.out.println("Maior de Idade");
+		} else {
+			System.out.println("Menor de Idade");
+		}
+
+	}
+
 }
